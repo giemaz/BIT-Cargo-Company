38f43fde0f085c6c8248a34bf4b3a6fb36ff4bb2
Giedrius <31162372+giemaz@users.noreply.github.com>
2023-04-14 10:46:23 +0300

    multer fix

--- server/routes/boxesRoutes.js
+++ server/routes/boxesRoutes.js
@@ -20,11 +20,11 @@ module.exports = (authenticateJWT) => {
 	});
 
 	// POST route
-	router.post('/boxes', authenticateJWT, (req, res) => {
+	router.post('/boxes', authenticateJWT, upload.single('image'), (req, res) => {
 		const newBox = {
 			weight: req.body.weight,
 			content: req.body.content,
-			image: req.body.image,
+			image: req.file ? req.file.path.replace(/\\/g, '/') : null,
 			is_flamable: req.body.is_flamable,
 			is_spoilable: req.body.is_spoilable,
 			container_id: req.body.container_id,
@@ -41,45 +41,87 @@ module.exports = (authenticateJWT) => {
 	});
 
 	// PUT route
-	router.put('/boxes/:id', authenticateJWT, (req, res) => {
+	router.put('/boxes/:id', authenticateJWT, upload.single('image'), (req, res) => {
 		const id = req.params.id;
-		const updatedBox = {
-			weight: req.body.weight,
-			content: req.body.content,
-			image: req.body.image,
-			is_flamable: req.body.is_flamable,
-			is_spoilable: req.body.is_spoilable,
-			container_id: req.body.container_id,
-		};
 
-		const sql = 'UPDATE boxes SET ? WHERE id = ?';
-		connection.query(sql, [updatedBox, id], (err, result) => {
+		connection.query('SELECT * FROM boxes WHERE id = ?', [id], (err, results) => {
 			if (err) {
-				res.status(500).json({ error: 'Error updating box' });
-			} else {
-				if (result.affectedRows === 0) {
-					res.status(404).json({ message: 'Box not found' });
-				} else {
-					res.status(200).json({ message: 'Box updated successfully', id: id });
-				}
+				res.status(500).json({ error: 'Error fetching box' });
+				return;
 			}
+
+			if (results.length === 0) {
+				res.status(404).json({ message: 'Box not found' });
+				return;
+			}
+
+			const box = results[0];
+			const oldImage = box.image;
+			const newImage = req.file ? req.file.path.replace(/\\/g, '/') : oldImage;
+
+			const updatedBox = {
+				weight: req.body.weight,
+				content: req.body.content,
+				image: newImage,
+				is_flamable: req.body.is_flamable,
+				is_spoilable: req.body.is_spoilable,
+				container_id: req.body.container_id,
+			};
+
+			const sql = 'UPDATE boxes SET ? WHERE id = ?';
+			connection.query(sql, [updatedBox, id], (err, result) => {
+				if (err) {
+					res.status(500).json({ error: 'Error updating box' });
+				} else {
+					if (result.affectedRows === 0) {
+						res.status(404).json({ message: 'Box not found' });
+					} else {
+						if (newImage !== oldImage && oldImage) {
+							fs.unlink(oldImage, (err) => {
+								if (err) console.error(err);
+							});
+						}
+						res.status(200).json({ message: 'Box updated successfully', id: id });
+					}
+				}
+			});
 		});
 	});
 
 	// DELETE route
 	router.delete('/boxes/:id', authenticateJWT, (req, res) => {
 		const id = req.params.id;
-		const sql = 'DELETE FROM boxes WHERE id = ?';
-		connection.query(sql, id, (err, result) => {
+
+		connection.query('SELECT * FROM boxes WHERE id = ?', [id], (err, results) => {
 			if (err) {
-				res.status(500).json({ error: 'Error deleting box' });
-			} else {
-				if (result.affectedRows === 0) {
-					res.status(404).json({ message: 'Box not found' });
-				} else {
-					res.status(200).json({ message: 'Box deleted successfully', id: id });
-				}
+				res.status(500).json({ error: 'Error fetching box' });
+				return;
 			}
+
+			if (results.length === 0) {
+				res.status(404).json({ message: 'Box not found' });
+				return;
+			}
+
+			const boxToDelete = results[0];
+			const imageToDelete = boxToDelete.image;
+
+			connection.query('DELETE FROM boxes WHERE id = ?', [id], (err, result) => {
+				if (err) {
+					res.status(500).json({ error: 'Error deleting box' });
+				} else {
+					if (result.affectedRows === 0) {
+						res.status(404).json({ message: 'Box not found' });
+					} else {
+						if (imageToDelete) {
+							fs.unlink(imageToDelete, (err) => {
+								if (err) console.error(err);
+							});
+						}
+						res.status(200).json({ message: 'Box deleted successfully', id: id });
+					}
+				}
+			});
 		});
 	});
 
